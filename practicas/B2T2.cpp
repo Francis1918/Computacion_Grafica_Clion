384b3f620eb6a33ada0831268ccd56fc7917b716
Francis1918 <9alemanbravo@gmail.com>
2025-12-16 10:27:15 -0500

    Acomodando para VS2022

--- practicas/B2T2.cpp
+++ practicas/B2T2.cpp
@@ -3,11 +3,11 @@
 //
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <shader_s.h>
-#include <stb_image.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "shader_s.h"
+#include "stb_image.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #include <vector>
 #include <iostream>
 #include <cmath>
